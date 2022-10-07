9773eda9d08414a90708d95b51c781becc339e02
ngthuc <lenguyenthuc.edu@gmail.com>
2022-10-07 18:50:42 +0700

    0.1.31

---
+++ src/index.d.ts
@@ -0,0 +1,28 @@
+import {Button, ButtonIconAndLabel, ButtonIcon, ButtonLabel, ButtonList} from "./Button";
+import {Card, CardHeader, CardContent, CardActions, CardFooter} from "./Card";
+import SignInWithEmail from "./SignInWithEmail";
+import SignInWithPhone from "./SignInWithPhone";
+import VerifyPhoneOTP from "./VerifyPhoneOTP";
+import * as Utils from "./utils";
+import {AUTH_TYPE, PROVIDER_TYPE} from "./types";
+import * as IMAGES from "./images";
+
+export {
+	SignInWithEmail,
+	SignInWithPhone,
+	VerifyPhoneOTP,
+	Button,
+	ButtonIconAndLabel,
+	ButtonIcon,
+	ButtonLabel,
+	ButtonList,
+	Card,
+	CardHeader,
+	CardContent,
+	CardActions,
+	CardFooter,
+	Utils,
+	AUTH_TYPE,
+	PROVIDER_TYPE,
+	IMAGES
+};
